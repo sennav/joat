0e51dd7fb7320cbffb09b831e910e0a38b11c56a
Vinicius <senna.vmd@gmail.com>
2019-07-03 22:08:11 +0200

    Avoid infinite loops in script commands.

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,8 @@ use std::collections::HashMap;
 use yaml_rust::Yaml;
 use std::process::Command;
 use std::io::{self, Write};
-use std::env;
+use std::{env, fs};
 use regex::Regex;
-use std::fs;
 use terminal_size::{Width, Height, terminal_size};
 use std::str::FromStr;
 
@@ -65,7 +64,30 @@ fn get_terminal_width() -> u16 {
     return 80;
 }
 
-fn execute_script(context: HashMap<String, HashMap<String,String>>, subcmd_yaml: &Yaml) {
+const RECUSRION_COUNT_VAR_NAME : &str = "DJOAT_RECURSION_COUNT";
+
+fn check_recursion_count(yaml: &Yaml) -> i64 {
+    let max_recursion_count;
+    if yaml["max_recursion_count"].is_badvalue() {
+        max_recursion_count = 100;
+    } else {
+        max_recursion_count = yaml["max_recursion_count"].clone().into_i64()
+            .expect("Max recursion should be an integer");
+    }
+    let recursion_count: i64 = match env::var(RECUSRION_COUNT_VAR_NAME) {
+        Ok(count) => count.parse()
+            .expect(&format!("{} should be of type i64", RECUSRION_COUNT_VAR_NAME)),
+        Err(_e) => 0,
+    };
+    if recursion_count > max_recursion_count {
+        println!("Max recursion count ({:?}) reached", max_recursion_count);
+        println!("Check for infinite loops in your yaml or increase max_recursion_count config");
+        ::std::process::exit(1);
+    }
+    return recursion_count + 1;
+}
+
+fn execute_script(context: HashMap<String, HashMap<String,String>>, subcmd_yaml: &Yaml, yaml: &Yaml,) {
     let script_string = subcmd_yaml["script"].clone().into_string()
         .expect("Could not convert script to string");
     let script = template::get_compiled_template_str_with_context(
@@ -73,10 +95,12 @@ fn execute_script(context: HashMap<String, HashMap<String,String>>, subcmd_yaml:
         &context)
         .expect(format!("Could not parse script template {:?}", script_string).as_str());
     let columns = get_terminal_width();
+    let recursion_count = check_recursion_count(yaml);
     let output = Command::new("bash")
             .arg("-c")
             .arg(script)
             .env("COLUMNS", columns.to_string())
+            .env(RECUSRION_COUNT_VAR_NAME, recursion_count.to_string())
             .output()
             .expect("failed to execute script");
     if !context["args"].contains_key("quiet") {
@@ -84,7 +108,7 @@ fn execute_script(context: HashMap<String, HashMap<String,String>>, subcmd_yaml:
         io::stderr().write_all(&output.stderr).unwrap();
     }
 
-    assert!(output.status.success());
+    ::std::process::exit(output.status.code().expect("Unknown exit code"));
 }
 
 fn get_parsed_yaml_key(key: &str, yaml: &Yaml, error_str: &str, context: &HashMap<String, HashMap<String, String>>) -> String {
@@ -203,7 +227,7 @@ fn execute(app_name: &String, cmd_name: &str, args: &ArgMatches, yaml: &Yaml) {
 
 
     if !script.is_badvalue() {
-        execute_script(context, &subcmd_yaml);
+        execute_script(context, &subcmd_yaml, &yaml);
     } else {
         execute_request(&app_name, &cmd_name, &yaml, &subcmd_yaml, context);
     }
